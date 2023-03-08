a0caa09155717b1b41980afe5f9a75f044e2c3ee
Hung-Hsi <jj830902@gmail.com>
2023-03-09 01:49:23 +0800

    zz

--- assets/js/colorbox-group.js
+++ assets/js/colorbox-group.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".group").colorbox({ rel: 'group', transition: "none", width: "75%", height: "75%" });
+    $(".group_sm").colorbox({ rel: 'group_sm', transition: "none", width: "75%", height: "75%" });
     $(".group_ot").colorbox({ rel: 'group_ot', transition: "none", width: "75%", height: "75%" });
     $(".group_menu").colorbox({ rel: 'group_menu', transition: "none", width: "75%", height: "75%" });
 });
